ec05adea7a59759f4f0bac09dc4b02896c38a99a
Moamal-2000 <103026211+Moamal-2000@users.noreply.github.com>
2024-01-18 13:53:33 +0300

    copy code feature

--- src/Components/hooks/HookCode.jsx
+++ src/Components/hooks/HookCode.jsx
@@ -1,12 +1,24 @@
 import { useEffect, useState } from "react";
 import HighlightElement from "react-highlight";
+import useCopyText from "../../Hooks/useCopyText";
+import useToggle from "../../Hooks/useToggle";
 import styles from "./HookCode.module.scss";
 
 const HookCode = ({ code }) => {
+  const [copiedText, copyText] = useCopyText();
+  const [isCopied, toggleIsCopied] = useToggle(false);
   const [numberOfLines, setNumberOfLines] = useState(0);
   const numbersOfLines = Array.from({ length: numberOfLines }).map(
     (_, i) => i + 1
   );
+
+  function handleCopyButton(e) {
+    if (isCopied) return
+    copyText(code);
+    toggleIsCopied()
+
+    setTimeout(() => toggleIsCopied(), 1000);
+  }
 
   useEffect(() => {
     let lines = code?.split("\n");
@@ -15,8 +27,16 @@ const HookCode = ({ code }) => {
 
   return (
     <div className={styles.code}>
-      <button className={styles.copyButton} title="copy icon">
-        <i class="fa-regular fa-copy"></i>
+      <button
+        className={styles.copyButton}
+        title="Copy Code"
+        onClick={(e) => handleCopyButton(e)}
+      >
+        {isCopied ? (
+          <i className="fa-solid fa-check"></i>
+        ) : (
+          <i className="fa-regular fa-copy"></i>
+        )}
       </button>
 
       <ul className={styles.numbering}>
