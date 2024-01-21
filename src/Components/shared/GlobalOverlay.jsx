3c29dacdcb75faf36df47163d3bdcdab7ae62bd9
Moamal-2000 <103026211+Moamal-2000@users.noreply.github.com>
2024-01-21 23:22:27 +0300

    close sidebar when click on the overlay

--- src/Components/shared/GlobalOverlay.jsx
+++ src/Components/shared/GlobalOverlay.jsx
@@ -2,11 +2,16 @@ import { useGlobalContext } from "../../Context/GlobalContext";
 import styles from "./GlobalOverlay.module.scss";
 
 const GlobalOverlay = () => {
-  const { isOverlayActive, setIsOverlayActive } = useGlobalContext();
+  const { isOverlayActive, setIsOverlayActive, setIsSideBarActive } = useGlobalContext();
+
+  function handleOverlayClick() {
+    setIsOverlayActive(false)
+    setIsSideBarActive(false)
+  }
 
   return (
     <div
-      onClick={() => setIsOverlayActive(false)}
+      onClick={handleOverlayClick}
       className={`${styles.overlay} ${isOverlayActive ? styles.active : ""}`}
     ></div>
   );
