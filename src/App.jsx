c12845e6677d9814da417797bf5c9dee2eadcb50
Moamal-2000 <103026211+Moamal-2000@users.noreply.github.com>
2024-01-26 02:39:35 +0300

    fix deployment on netlify #5

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import Footer from "./Components/footer/Footer";
 import Home from "./Components/home/Home";
-import GlobalOverlay from "./Components/shared/GlobalOverlay";
+// import GlobalOverlay from "./Components/shared/GlobalOverlay";
 import { useGlobalContext } from "./Context/GlobalContext";
 // import ScrollBottomTop from "./Components/Shared/Features/ScrollBottomTop";
 // import FullScreenMode from "./Components/Shared/Features/FullScreenMode";
@@ -25,7 +25,7 @@ function App() {
       )}
 
       <FocusMode />
-      <GlobalOverlay />
+      {/* <GlobalOverlay /> */}
     </>
   );
 }
