81b9fbd558024ad26b81a143c7aa7b76ac11d78c
Moamal-2000 <103026211+Moamal-2000@users.noreply.github.com>
2024-01-26 02:32:28 +0300

    fix deployment on netlify #3

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,9 @@ import Footer from "./Components/footer/Footer";
 import Home from "./Components/home/Home";
 import GlobalOverlay from "./Components/shared/GlobalOverlay";
 import { useGlobalContext } from "./Context/GlobalContext";
-import ScrollBottomTop from "./Components/Shared/Features/ScrollBottomTop";
-import FullScreenMode from "./Components/Shared/Features/FullScreenMode";
-import FocusMode from "./Components/Shared/Features/FocusMode";
+// import ScrollBottomTop from "./Components/Shared/Features/ScrollBottomTop";
+// import FullScreenMode from "./Components/Shared/Features/FullScreenMode";
+// import FocusMode from "./Components/Shared/Features/FocusMode";
 
 function App() {
   const { isFocusModeActive } = useGlobalContext();
@@ -19,12 +19,12 @@ function App() {
 
       {!isFocusModeActive && (
         <>
-          <ScrollBottomTop />
-          <FullScreenMode />
+          {/* <ScrollBottomTop /> */}
+          {/* <FullScreenMode /> */}
         </>
       )}
 
-      <FocusMode />
+      {/* <FocusMode /> */}
       <GlobalOverlay />
     </>
   );
