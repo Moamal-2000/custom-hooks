24a791b5b81482702bad81f07a6bb2605459a0dc
Moamal-2000 <103026211+Moamal-2000@users.noreply.github.com>
2024-01-30 06:43:12 +0300

    Testing change folder names

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,8 @@ import FocusMode from "./Components/Shared/Features/FocusMode";
 import FullScreenMode from "./Components/Shared/Features/FullScreenMode";
 import ScrollBottomTop from "./Components/Shared/Features/ScrollBottomTop";
 import GlobalOverlay from "./Components/Shared/GlobalOverlay";
-import Footer from "./Components/footer/Footer";
-import Home from "./Components/home/Home";
+import Footer from "./Components/Footer/Footer";
+import Home from "./Components/Home/Home";
 import { useGlobalContext } from "./Context/GlobalContext";
 
 function App() {
