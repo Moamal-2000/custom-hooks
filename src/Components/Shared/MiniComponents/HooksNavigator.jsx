794eb5e9053e2937a8afeedffaeccf0ddcdcb6f5
Moamal-2000 <103026211+Moamal-2000@users.noreply.github.com>
2024-01-31 10:57:10 +0300

    fix next page button

--- src/Components/Shared/MiniComponents/HooksNavigator.jsx
+++ src/Components/Shared/MiniComponents/HooksNavigator.jsx
@@ -5,13 +5,14 @@ import { scrollToBottom } from "../../../Functions/helper";
 const HooksNavigator = () => {
   const { numbersOfPages } = useGlobalContext();
   const location = useLocation();
-  const currentPage = location.pathname.slice(1, 2);
+  let currentPage = location.pathname.slice(1, 2);
   const navigateTo = useNavigate();
   const lengthPages = numbersOfPages?.length;
   const isLastPage = lengthPages !== +currentPage;
   const isFirstPage = +currentPage > 1;
 
   function handleNextPage() {
+    if (!currentPage) currentPage = 1
     if (isLastPage) navigateTo(`${+currentPage + 1}`);
     scrollToBottom();
   }
