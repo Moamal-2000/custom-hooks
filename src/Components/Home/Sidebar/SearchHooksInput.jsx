0ea75002f1b359838d985227be348106c9628190
Moamal-2000 <103026211+Moamal-2000@users.noreply.github.com>
2024-02-01 15:08:09 +0300

    build handleSearch logic #1

--- src/Components/Home/Sidebar/SearchHooksInput.jsx
+++ src/Components/Home/Sidebar/SearchHooksInput.jsx
@@ -1,13 +1,49 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { useGlobalContext } from "../../../Context/GlobalContext";
+import { hooksData } from "../../../Data/hooksData";
 import styles from "./SearchHooksInput.module.scss";
 
 const SearchHooksInput = () => {
+  const [searchInp, setSearchInp] = useState("");
+  const navigateTo = useNavigate();
+  const { setIsSideBarActive, setIsOverlayActive } = useGlobalContext();
+
+  function handleSearch(e) {
+    e.preventDefault();
+
+    const filteredResults = hooksData.filter((hookData) =>
+      hookData.name.startsWith(searchInp)
+    );
+
+    if (filteredResults === 0) return;
+
+    if (filteredResults.length === 1) {
+      navigateTo(`/${filteredResults[0].page}`);
+    }
+
+    if (filteredResults.length > 1) {
+      
+    }
+
+    setIsOverlayActive(false);
+    setIsSideBarActive(false);
+  }
+
   return (
     <form
       className={styles.SearchForm}
-      onSubmit={(e) => e.preventDefault()}
+      onSubmit={(e) => handleSearch(e)}
       role="search"
     >
-      <input id="search" type="search" placeholder="Search" autofocus />
+      <input
+        id="search"
+        type="search"
+        placeholder="Search"
+        autoComplete="off"
+        value={searchInp}
+        onChange={(e) => setSearchInp(e.target.value)}
+      />
 
       <button type="submit">Go</button>
     </form>
