a9cedfd54179c1d177c21aa4d02ef0990904a56f
Moamal-2000 <103026211+Moamal-2000@users.noreply.github.com>
2024-02-03 18:27:15 +0300

    built suggestion menu for search input, still in development phase

--- src/Components/Sidebar/SearchHooksInput.jsx
+++ src/Components/Sidebar/SearchHooksInput.jsx
@@ -1,64 +1,91 @@
-import { useState } from "react";
+import { useRef } from "react";
 import { useNavigate } from "react-router-dom";
 import { useGlobalContext } from "../../Context/GlobalContext";
 import { hooksData } from "../../Data/hooksData";
+import useArray from "../../Hooks/useArray";
+import useToggle from "../../Hooks/useToggle";
 import styles from "./SearchHooksInput.module.scss";
 
 const SearchHooksInput = () => {
-  const [searchInp, setSearchInp] = useState("");
+  const searchInpRef = useRef();
   const navigateTo = useNavigate();
-  const { setIsSideBarActive, setIsOverlayActive } =
-    useGlobalContext();
+  const { setIsSideBarActive, setIsOverlayActive } = useGlobalContext();
+  const { array: searchItems, set: setSearchItems } = useArray([]);
+  const [isSuggestionMenuActive, _, setSuggestionsActive] = useToggle();
 
   function handleSearch(e) {
     e.preventDefault();
 
     const filteredResults = hooksData.filter((hookData) =>
-      hookData.name.toLowerCase().startsWith(searchInp.toLowerCase())
+      hookData.name
+        .toLowerCase()
+        .startsWith(searchInpRef.current?.value?.toLowerCase())
     );
+    const isNotFound = filteredResults === 0;
+    const isFound = filteredResults.length === 1;
 
-    if (filteredResults === 0) return;
-
-    if (filteredResults.length === 1) {
-      navigateTo(`/?page=${filteredResults[0].page}`);
-
-      setTimeout(() => {
-        const sidebarLinksHooks =
-          document.querySelectorAll("aside>div>ul>li>a");
-
-        sidebarLinksHooks.forEach((hookLink) => {
-          if (hookLink.textContent === filteredResults[0].name)
-            hookLink.click();
-        });
-      }, 200);
-
-      setSearchInp("")
-    }
-
+    if (isNotFound) return;
+    if (isFound) navigateToItem(filteredResults[0])
     if (filteredResults.length > 1) {
+      setSuggestionsActive(true);
+      setSearchItems(filteredResults);
     }
 
     setIsOverlayActive(false);
     setIsSideBarActive(false);
   }
 
-  return (
-    <form
-      className={styles.SearchForm}
-      onSubmit={(e) => handleSearch(e)}
-      role="search"
-    >
-      <input
-        id="search"
-        type="search"
-        placeholder="Search"
-        autoComplete="off"
-        value={searchInp}
-        onChange={(e) => setSearchInp(e.target.value)}
-      />
+  function navigateToItem(itemData) {
+    navigateTo(`/?page=${itemData.page}`);
 
-      <button type="submit">Go</button>
-    </form>
+    setTimeout(() => {
+      const sidebarLinksHooks =
+        document.querySelectorAll("aside>div>ul>li>a");
+
+      sidebarLinksHooks.forEach((hookLink) => {
+        if (hookLink.textContent === itemData.name)
+          hookLink.click();
+      });
+    }, 200);
+
+    setSearchInp("");
+  }
+
+  return (
+    <>
+      <form
+        className={styles.SearchForm}
+        onSubmit={(e) => handleSearch(e)}
+        role="search"
+      >
+        <input
+          id="search"
+          type="search"
+          placeholder="Search"
+          autoComplete="off"
+          ref={searchInpRef}
+        />
+
+        <button type="submit">Go</button>
+      </form>
+
+      <ul
+        className={`${styles.suggestionMenu} ${
+          isSuggestionMenuActive ? styles.active : ""
+        }`}
+      >
+        {searchItems?.map((item) => (
+          <li
+            key={item.id}
+            className={styles.searchItem}
+            onClick={() => navigateToItem(item)}
+          >
+            <span>{item.name}</span>
+            <i className="fa-solid fa-magnifying-glass"></i>
+          </li>
+        ))}
+      </ul>
+    </>
   );
 };
 export default SearchHooksInput;
