70241b6bc664f5af17b471e637a77cbd530396dc
Moamal-2000 <103026211+Moamal-2000@users.noreply.github.com>
2024-02-08 21:32:34 +0300

    testing plugin postcss for css auto prefix

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,16 @@
 import react from "@vitejs/plugin-react-swc";
+import autoprefixer from "autoprefixer";
+import postcss from "postcss";
 import { defineConfig } from "vite";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    postcss({
+      plugins: [autoprefixer],
+      config: "./postcss.config.cjs",
+    }),
+  ],
   build: {
     sourcemap: true,
   },
