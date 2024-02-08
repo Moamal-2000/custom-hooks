2286590ae2c016b9f9459e3f0420dd291e9e47d7
Moamal-2000 <103026211+Moamal-2000@users.noreply.github.com>
2024-02-08 14:31:10 +0300

    renamed GlobalStyles with Styles

--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,8 @@ import React, { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import GlobalContextProvider from "./Context/GlobalContext";
-import "./GlobalStyles/colors.scss";
-import "./GlobalStyles/index.scss";
+import "./Styles/colors.scss";
+import "./Styles/index.scss";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <StrictMode>
