d2704bb350bfcf3057991b7016ef47b195510591
Moamal-2000 <103026211+Moamal-2000@users.noreply.github.com>
2024-02-21 23:17:58 +0300

    modified manifest.js

--- manifest.js
+++ manifest.js
@@ -1,6 +1,5 @@
 export const manifestForPlugin = {
   registerType: "prompt",
-  includeAssests: ["favicon.ico", "apple-touc-icon.png"],
   manifest: {
     name: "Hooks time",
     short_name: "Hooks time",
@@ -14,19 +13,16 @@ export const manifestForPlugin = {
         src: "/android-chrome-192x192.png",
         sizes: "192x192",
         type: "image/png",
-        purpose: "favicon",
       },
       {
         src: "/android-chrome-512x512.png",
         sizes: "512x512",
         type: "image/png",
-        purpose: "favicon",
       },
       {
         src: "/apple-touch-icon.png",
         sizes: "180x180",
         type: "image/png",
-        purpose: "apple touch icon",
       },
     ],
     theme_color: "#181818",
