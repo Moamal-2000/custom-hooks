bf1d03aded6d14ed9de6e084ca55f371e3513225
Moamal-2000 <103026211+Moamal-2000@users.noreply.github.com>
2024-02-21 23:09:58 +0300

    setup PWA

---
+++ manifest.js
@@ -0,0 +1,39 @@
+export const manifestForPlugin = {
+  registerType: "prompt",
+  includeAssests: ["favicon.ico", "apple-touc-icon.png"],
+  manifest: {
+    name: "Hooks time",
+    short_name: "Hooks time",
+    description: `Explore a treasure trove of custom React hooks on our website, 
+    each accompanied by detailed explanations, live code examples, 
+    and input/output demonstrations. Copy and download the code effortlessly, 
+    and stay tuned as we regularly add new hooks. Enhance your React projects with our comprehensive collection. 
+    Download all hooks as a convenient RAR file for seamless integration.`,
+    icons: [
+      {
+        src: "/android-chrome-192x192.png",
+        sizes: "192x192",
+        type: "image/png",
+        purpose: "favicon",
+      },
+      {
+        src: "/android-chrome-512x512.png",
+        sizes: "512x512",
+        type: "image/png",
+        purpose: "favicon",
+      },
+      {
+        src: "/apple-touch-icon.png",
+        sizes: "180x180",
+        type: "image/png",
+        purpose: "apple touch icon",
+      },
+    ],
+    theme_color: "#181818",
+    background_color: "#e0cc3b",
+    display: "standalone",
+    scope: "/",
+    start_url: "/",
+    orientation: "portrait",
+  },
+};
