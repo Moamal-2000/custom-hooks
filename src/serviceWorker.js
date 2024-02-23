1921d6c49442b96ee84675ece39d42dcb2df793a
Moamal-2000 <103026211+Moamal-2000@users.noreply.github.com>
2024-02-23 14:08:29 +0300

    modified serviceWorker

--- src/serviceWorker.js
+++ src/serviceWorker.js
@@ -2,21 +2,6 @@ export function register(config) {
   if (process.env.NODE_ENV === "production" && "serviceWorker" in navigator) {
     const publicUrl = new URL(process.env.PUBLIC_URL, window.location.href);
     if (publicUrl.origin !== window.location.origin) return;
-
-    window.addEventListener("load", () => {
-      const swUrl = `${process.env.PUBLIC_URL}/service-worker.js`;
-
-      if (isLocalhost) {
-        checkValidServiceWorker(swUrl, config);
-
-        navigator.serviceWorker.ready.then(() => {
-          console.log(
-            "This web app is being served cache-first by a service " +
-              "worker. To learn more, visit https://cra.link/PWA"
-          );
-        });
-      } else registerValidSW(swUrl, config);
-    });
   }
 }
 
