a413eb64f10237c67f605db4e2ad5df9d242ee0a
Moamal-2000 <103026211+Moamal-2000@users.noreply.github.com>
2024-02-24 12:18:26 +0300

    again

--- src/serviceWorkerRegistration.js
+++ src/serviceWorkerRegistration.js
@@ -37,7 +37,7 @@ export function register(config) {
 
     window.addEventListener("load", () => {
       console.log(publicUrl);
-      const swUrl = `${publicUrl.href}/service-worker.js`;
+      const swUrl = `/service-worker.js`;
       console.log('swUrl', swUrl);
 
       if (isLocalhost) {
