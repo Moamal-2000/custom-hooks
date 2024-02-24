15e9f3c0fdd75c75a06df311ad344855fbe82446
Moamal-2000 <103026211+Moamal-2000@users.noreply.github.com>
2024-02-24 12:09:33 +0300

    modified publicUrl

--- src/serviceWorkerRegistration.js
+++ src/serviceWorkerRegistration.js
@@ -27,7 +27,7 @@ export function register(config) {
     "serviceWorker" in navigator
   ) {
     // The URL constructor is available in all browsers that support SW.
-    const publicUrl = new URL(process.env.PUBLIC_URL, window.location.href);
+    const publicUrl = new URL(window.location.href);
     if (publicUrl.origin !== window.location.origin) {
       // Our service worker won't work if PUBLIC_URL is on a different origin
       // from what our page is served on. This might happen if a CDN is used to
