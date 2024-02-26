11ece82cefc3b58543a8740571f1b48a90544c5b
Moamal-2000 <103026211+Moamal-2000@users.noreply.github.com>
2024-02-26 11:27:49 +0300

    trying fix: Manifest: found icon with no valid size.

--- manifest.js
+++ manifest.js
@@ -42,7 +42,7 @@ export const manifestForPlugin = {
       },
       {
         src: "/favicon.ico",
-        sizes: "32x32",
+        sizes: "64x64 32x32 24x24 16x16",
         type: "image/x-icon",
       },
       {
