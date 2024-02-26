74e08fee1f1bc81d4d350ef648ab050af162237d
Moamal-2000 <103026211+Moamal-2000@users.noreply.github.com>
2024-02-26 11:30:35 +0300

    trying fix the icon that cause the problem

--- manifest.js
+++ manifest.js
@@ -30,26 +30,26 @@ export const manifestForPlugin = {
         type: "image/webp",
         purpose: "maskable",
       },
-      {
-        src: "/android-chrome-512x512.png",
-        sizes: "512x512",
-        type: "image/png",
-      },
-      {
-        src: "/favicon-16x16.webp",
-        sizes: "16x16",
-        type: "image/webp",
-      },
-      {
-        src: "/favicon.ico",
-        sizes: "64x64 32x32 24x24 16x16",
-        type: "image/x-icon",
-      },
-      {
-        src: "/favicon-32x32.webp",
-        sizes: "32x32",
-        type: "image/webp",
-      },
+      // {
+      //   src: "/android-chrome-512x512.png",
+      //   sizes: "512x512",
+      //   type: "image/png",
+      // },
+      // {
+      //   src: "/favicon-16x16.webp",
+      //   sizes: "16x16",
+      //   type: "image/webp",
+      // },
+      // {
+      //   src: "/favicon.ico",
+      //   sizes: "64x64 32x32 24x24 16x16",
+      //   type: "image/x-icon",
+      // },
+      // {
+      //   src: "/favicon-32x32.webp",
+      //   sizes: "32x32",
+      //   type: "image/webp",
+      // },
     ],
     theme_color: "#181818",
     background_color: "#181818",
