66712ba63ea43a93208579ffdbb11b5df43066e7
Moamal-2000 <103026211+Moamal-2000@users.noreply.github.com>
2024-02-26 11:16:43 +0300

    stopped servicerWorker

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,5 @@
 import React, { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
-import * as serviceWorker from "../registerPWA";
 import App from "./App";
 import GlobalContextProvider from "./Context/GlobalContext";
 import "./Styles/colors.scss";
@@ -13,5 +12,3 @@ ReactDOM.createRoot(document.getElementById("root")).render(
     </GlobalContextProvider>
   </StrictMode>
 );
-
-serviceWorker.register();
