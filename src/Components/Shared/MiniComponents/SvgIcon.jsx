1d53c6cdf6308842bdf717b761d431c58cbffb50
Moamal-2000 <103026211+Moamal-2000@users.noreply.github.com>
2024-10-05 10:14:25 +0300

    perf: improve SvgIcon component code

--- src/Components/Shared/MiniComponents/SvgIcon.jsx
+++ src/Components/Shared/MiniComponents/SvgIcon.jsx
@@ -1,8 +1,18 @@
+import { useMemo } from "react";
 import { iconsData } from "src/Data/iconsData";
 
 const SvgIcon = ({ name }) => {
-  const iconData = iconsData.find((iconData) => iconData.name === name);
-  return iconData && iconData?.icon;
+  const iconData = useMemo(
+    () => iconsData.find((iconData) => iconData?.name === name),
+    [name]
+  );
+
+  if (!iconData) {
+    console.warn(`Icon with name "${name}" not found.`);
+    return null;
+  }
+
+  return iconData?.icon;
 };
 
 export default SvgIcon;
